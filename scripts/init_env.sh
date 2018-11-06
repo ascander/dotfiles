7fb5333f0379b25d80d2eb75c35d4c4c951c9712
Cody Allen <ceedubs@gmail.com>
2018-11-06 13:08:48 -0800

    Fix Droid Sans Mono URL

--- scripts/init_env.sh
+++ scripts/init_env.sh
@@ -1,6 +1,6 @@
 # install the font that I like
 if [ ! -f $HOME/Library/Fonts/Droid+Sans+Mono+for+Powerline.otf ]; then
-  curl --create-dirs -o $HOME/Library/Fonts/Droid+Sans+Mono+for+Powerline.otf 'https://github.com/powerline/fonts/raw/c320e67b6564b9bf3c76aade296d47b57ca6d529/DroidSansMono/Droid%20Sans%20Mono%20for%20Powerline.otf'
+  curl --create-dirs -o $HOME/Library/Fonts/Droid+Sans+Mono+for+Powerline.otf 'https://raw.githubusercontent.com/powerline/fonts/c320e67b6564b9bf3c76aade296d47b57ca6d529/DroidSansMono/Droid%20Sans%20Mono%20for%20Powerline.otf'
 fi
 
 # only show terminal scrollbar when scrolling
