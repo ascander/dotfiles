f3e7b1f0684af385fc19342957ec2948cd639db0
Cody Allen <ceedubs@gmail.com>
2019-01-22 14:34:07 -0800

    Separate out custom tools

--- default.nix
+++ default.nix
@@ -5,8 +5,65 @@ let
 
   pkgs = import <nixpkgs> { };
 
+  custom = rec {
+    # A custom '.bashrc' (see bashrc/default.nix for details)
+    bashrc = pkgs.callPackage ./bashrc {};
+
+    # Git with config baked in
+    git = import ./git (
+      { inherit (pkgs) makeWrapper symlinkJoin;
+        git = pkgs.git;
+      });
+
+    # silver-searcher with environment setup
+    silver-searcher = import ./silver-searcher (
+      { inherit (pkgs) makeWrapper symlinkJoin;
+        silver-searcher = pkgs.silver-searcher;
+      });
+
+    # custom prezto
+    zsh-prezto = import ./zsh/prezto.nix (
+      { inherit (pkgs) stdenv fetchgit; }
+    );
+
+    # Zsh with config baked in
+    zsh = import ./zsh (
+      { inherit (pkgs) makeWrapper symlinkJoin;
+        zsh = pkgs.zsh;
+        zsh-prezto = zsh-prezto;
+      });
+
+    # Tmux with a custom tmux.conf baked in
+    tmux = import ./tmux (with pkgs;
+      { inherit
+          makeWrapper
+          symlinkJoin
+          ;
+        tmux = pkgs.tmux;
+      });
+
+    vim = import ./vim (
+      {
+        neovim = pkgs.neovim;
+        vimPlugins = pkgs.vimPlugins;
+        buildVimPluginFrom2Nix = pkgs.vimUtils.buildVimPluginFrom2Nix;
+        fetchFromGitHub = pkgs.fetchFromGitHub;
+      });
+
+    emacs = import ./emacs (with pkgs;
+      { inherit
+          makeWrapper
+          symlinkJoin
+        ;
+        emacsWithPackages = (pkgs.emacsPackagesNgGen pkgs.emacs).emacsWithPackages;
+        epkgs = pkgs.epkgs.melpaStablePackages;
+      });
+
+      metals = pkgs.callPackage ./metals {};
+  };
+
   # The list of packages to be installed
-  tools = with pkgs;
+  tools = with pkgs; with custom;
     [
       # Customized packages
       bashrc
@@ -18,76 +75,21 @@ let
       emacs
       metals
 
-      pkgs.bash
-      pkgs.cacert
-      pkgs.gnupg
-      pkgs.pinentry
-      pkgs.reattach-to-user-namespace
-      pkgs.zsh-completions
-      pkgs.gawk
-      pkgs.less
-      pkgs.nix
-      pkgs.tree
-      pkgs.gitAndTools.hub
-      pkgs.openjdk
+      bash
+      cacert
+      gnupg
+      pinentry
+      reattach-to-user-namespace
+      zsh-completions
+      gawk
+      less
+      nix
+      tree
+      gitAndTools.hub
+      openjdk
     ];
 
   ## Some customizations
-
-  # A custom '.bashrc' (see bashrc/default.nix for details)
-  bashrc = pkgs.callPackage ./bashrc {};
-
-  # Git with config baked in
-  git = import ./git (
-    { inherit (pkgs) makeWrapper symlinkJoin;
-      git = pkgs.git;
-    });
-
-  # silver-searcher with environment setup
-  silver-searcher = import ./silver-searcher (
-    { inherit (pkgs) makeWrapper symlinkJoin;
-      silver-searcher = pkgs.silver-searcher;
-    });
-
-  # custom prezto
-  zsh-prezto = import ./zsh/prezto.nix (
-    { inherit (pkgs) stdenv fetchgit; }
-  );
-
-  # Zsh with config baked in
-  zsh = import ./zsh (
-    { inherit (pkgs) makeWrapper symlinkJoin;
-      zsh = pkgs.zsh;
-      zsh-prezto = zsh-prezto;
-    });
-
-  # Tmux with a custom tmux.conf baked in
-  tmux = import ./tmux (with pkgs;
-    { inherit
-        makeWrapper
-        symlinkJoin
-        ;
-      tmux = pkgs.tmux;
-    });
-
-  vim = import ./vim (
-    {
-      neovim = pkgs.neovim;
-      vimPlugins = pkgs.vimPlugins;
-      buildVimPluginFrom2Nix = pkgs.vimUtils.buildVimPluginFrom2Nix;
-      fetchFromGitHub = pkgs.fetchFromGitHub;
-    });
-
-  emacs = import ./emacs (with pkgs;
-    { inherit
-        makeWrapper
-        symlinkJoin
-      ;
-      emacsWithPackages = (pkgs.emacsPackagesNgGen pkgs.emacs).emacsWithPackages;
-      epkgs = pkgs.epkgs.melpaStablePackages;
-    });
-
-    metals = pkgs.callPackage ./metals {};
 
 in
   if pkgs.lib.inNixShell
