87e2ac678005ab7a85245689080fb82a43f04055
Cody Allen <ceedubs@gmail.com>
2019-01-23 11:10:54 -0800

    Use a pinned version of nixpkgs

--- default.nix
+++ default.nix
@@ -3,7 +3,15 @@
 # Based on https://github.com/nmattia/homies/blob/master/default.nix
 let
 
-  pkgs = import <nixpkgs> { };
+  # use a pinned version of nixpkgs for reproducability
+  nixpkgs-version = "18.09";
+  pkgs = import (builtins.fetchTarball {
+    # Descriptive name to make the store path easier to identify
+    name = "nixpkgs-${nixpkgs-version}";
+    url = "https://github.com/nixos/nixpkgs/archive/${nixpkgs-version}.tar.gz";
+    # Hash obtained using `nix-prefetch-url --unpack <url>`
+    sha256 = "1ib96has10v5nr6bzf7v8kw7yzww8zanxgw2qi1ll1sbv6kj6zpd";
+  }) {};
 
   custom = rec {
     # A custom '.bashrc' (see bashrc/default.nix for details)
