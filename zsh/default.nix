6dd4b263fe26d7678a35bfce73973b8c2d6740d1
Cody Allen <ceedubs@gmail.com>
2019-02-23 08:08:31 -0800

    Clean up zsh completion handling

--- zsh/default.nix
+++ zsh/default.nix
@@ -1,5 +1,5 @@
 # Zsh with conf files baked in
-{ zsh, symlinkJoin, makeWrapper, pure-prompt, ddgr, zsh-syntax-highlighting, hub }:
+{ zsh, symlinkJoin, makeWrapper, zsh-syntax-highlighting, lib, site-functions }:
 symlinkJoin {
   name = "zsh";
   buildInputs = [ makeWrapper ];
@@ -7,9 +7,7 @@ symlinkJoin {
   postBuild = ''
     cp ${./conf}/.zshrc $out/.zshrc
     mkdir -p "$out/site-functions"
-    cp ${pure-prompt}/share/zsh/site-functions/* $out/site-functions/ -R
-    cp ${ddgr}/share/zsh/site-functions/* $out/site-functions/ -R
-    cp ${hub}/share/zsh/site-functions/* $out/site-functions/ -R
+    ${lib.concatMapStrings (s: "cp ${s}/* $out/site-functions/ -R;") site-functions}
     cp ${zsh-syntax-highlighting}/share/zsh-syntax-highlighting $out/ -R
     wrapProgram "$out/bin/zsh" \
     --set ZDOTDIR "$out"
