f13714cc3d576b74b77cd5756c8474a4e900a23f
Ascander Dost <dostinthemachine@gmail.com>
2019-03-27 21:42:21 -0700

    Adds config for screenshot directory

--- scripts/init_env.sh
+++ scripts/init_env.sh
@@ -8,3 +8,13 @@ defaults write com.apple.Terminal AppleShowScrollBars -string WhenScrolling
 
 # don't make annoying sounds when emptying trash or taking a screenshot
 defaults write com.apple.systemsound "com.apple.sound.uiaudio.enabled" -int 0
+
+# don't litter the desktop with screenshots
+SCREENSHOTS_DIR="${HOME}/Pictures/Screenshots"
+
+if [ ! -d "$SCREENSHOTS_DIR" ]; then
+    echo "Creating directory: $SCREENSHOTS_DIR for screenshots"
+    mkdir -p "$SCREENSHOTS_DIR"
+fi
+
+defaults write com.apple.screencapture location "$SCREENSHOTS_DIR"
