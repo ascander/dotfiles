d27038bdbc0f283f0bf7a973ab379620781387a2
Ascander Dost <1815984+ascander@users.noreply.github.com>
2024-02-20 12:09:22 -0800

    Formats script (testing none-ls)

--- bin/switchtheme.sh
+++ bin/switchtheme.sh
@@ -52,69 +52,72 @@ ALACRITTY_THEMES_DIR="${ALACRITTY_CONFIG_DIR}/themes"
 
 # Print usage information
 usage() {
-  echo "Switch Alacritty color theme"
-  echo ""
-  echo "Usage:"
-  echo "  $(basename "$0") [OPTIONS] THEME"
-  echo ""
-  echo "Options:"
-  echo "  -h  Display this message"
-  echo "  -l  List available themes"
-  echo "  -f  Delete backup Alacritty config file"
+	echo "Switch Alacritty color theme"
+	echo ""
+	echo "Usage:"
+	echo "  $(basename "$0") [OPTIONS] THEME"
+	echo ""
+	echo "Options:"
+	echo "  -h  Display this message"
+	echo "  -l  List available themes"
+	echo "  -f  Delete backup Alacritty config file"
 }
 
 # List available themes
 list_themes() {
-  local available_themes=()
-  for file in `ls "$ALACRITTY_THEMES_DIR"`; do
-    available_themes+=( "${file%.toml}" )
-  done
+	local available_themes=()
+	for file in $(ls "$ALACRITTY_THEMES_DIR"); do
+		available_themes+=("${file%.toml}")
+	done
 
-  echo "Available themes:"
-  echo ""
-  for theme in "${available_themes[@]}"; do
-    echo "  ${theme}"
-  done
+	echo "Available themes:"
+	echo ""
+	for theme in "${available_themes[@]}"; do
+		echo "  ${theme}"
+	done
 }
 
 # Main function
 main() {
-  while getopts "hlf" opt; do
-    case "$opt" in
-      l)
-        list_themes
-        exit;;
-      f)
-        DELETE_BACKUP=1;;
-      h | *)
-        usage
-        exit;;
-    esac
-  done
-  shift $((OPTIND-1))
+	while getopts "hlf" opt; do
+		case "$opt" in
+		l)
+			list_themes
+			exit
+			;;
+		f)
+			DELETE_BACKUP=1
+			;;
+		h | *)
+			usage
+			exit
+			;;
+		esac
+	done
+	shift $((OPTIND - 1))
 
-  local alacritty_config_file="${ALACRITTY_CONFIG_DIR}/alacritty.toml" # the config file
-  local desired_theme="$1"                                             # the theme you want
-  local available_themes=()                                            # init available themes array
+	local alacritty_config_file="${ALACRITTY_CONFIG_DIR}/alacritty.toml" # the config file
+	local desired_theme="$1"                                             # the theme you want
+	local available_themes=()                                            # init available themes array
 
-  # Collect available themes
-  for file in `ls "$ALACRITTY_THEMES_DIR"`; do
-    available_themes+=( "${file%.toml}" )
-  done
+	# Collect available themes
+	for file in $(ls "$ALACRITTY_THEMES_DIR"); do
+		available_themes+=("${file%.toml}")
+	done
 
-  # Validate desired theme
-  if [[ ! " ${available_themes[@]} " =~ [[:space:]]${desired_theme}[[:space:]] ]]; then
-    echo "Unrecognized theme: ${desired_theme}. Exiting."
-    exit 1
-  fi
+	# Validate desired theme
+	if [[ ! " ${available_themes[@]} " =~ [[:space:]]${desired_theme}[[:space:]] ]]; then
+		echo "Unrecognized theme: ${desired_theme}. Exiting."
+		exit 1
+	fi
 
-  # Update config file
-  sed -i'.bk' -r "s/(themes\/)[a-z_-]+(\.toml)/\1${desired_theme}\2/" "$alacritty_config_file"
+	# Update config file
+	sed -i'.bk' -r "s/(themes\/)[a-z_-]+(\.toml)/\1${desired_theme}\2/" "$alacritty_config_file"
 
-  # Cleanup if requested
-  if [[ $DELETE_BACKUP -gt 0 ]]; then
-    [ ! -s "${alacritty_config_file}.bk" ] || rm -f "${alacritty_config_file}.bk"
-  fi
+	# Cleanup if requested
+	if [[ $DELETE_BACKUP -gt 0 ]]; then
+		[ ! -s "${alacritty_config_file}.bk" ] || rm -f "${alacritty_config_file}.bk"
+	fi
 }
 
 main "$@"
